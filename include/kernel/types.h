663c0cf92b0cf29a700acf1f2e813ba5ae5559cf
RAGHAVENDRA RONGALI <38320603+muthyalanaidu987@users.noreply.github.com>
2019-02-24 19:28:10 +0530

    Add files via upload

---
+++ include/kernel/types.h
@@ -0,0 +1,37 @@
+#ifndef KERNELTYPES_H
+#define KERNELTYPES_H
+
+#pragma pack(2)
+
+#define PAGE_SIZE 4096
+#define PAGE_BITS 12
+#define PAGE_MASK 0xfffff000
+
+#define KILO 1024
+#define MEGA (KILO*KILO)
+#define GIGA (KILO*KILO*KILO)
+
+#define MAX_ARGV_LENGTH 256
+
+#define MIN(x,y) ( ((x)<(y)) ? (x) : (y) )
+#define MAX(x,y) ( ((x)>(y)) ? (x) : (y) )
+
+typedef int int32_t;
+typedef short int16_t;
+typedef char int8_t;
+
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+
+typedef uint8_t bool;
+
+typedef uint32_t addr_t;
+
+struct process_info {
+	int pid;
+	int exitcode;
+	int exitreason;
+};
+
+#endif
